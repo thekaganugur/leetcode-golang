0b70df7fe095d86d2cb7f263275138b11db11abb
Kağan Uğur <kgnugur@gmail.com>
2019-10-19 18:19:57 +0300

    Remove Duplicates from Sorted List

---
+++ 83.remove-duplicates-from-sorted-list.go
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=83 lang=golang
+ *
+ * [83] Remove Duplicates from Sorted List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+package main
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	curr := head
+	for curr != nil && curr.Next != nil {
+		if curr.Val == curr.Next.Val {
+			curr.Next = curr.Next.Next
+		} else {
+			curr = curr.Next
+		}
+	}
+	return head
+}
+
+// @lc code=end
